57db3e2406533cba5aecbfc7be77ead84c56100b
VatoAi <HorusNorway@proton.me>
2025-04-29 16:02:46 +0000

    refactor: Consolidate imports in media encryption module for clarity

--- src/utils/encryption/media/index.ts
+++ src/utils/encryption/media/index.ts
@@ -1,5 +1,5 @@
-
-import { encryptMedia } from './encrypt';
+import { encryptMedia, encryptFile } from './encrypt';
 export * from './encrypt';
 export * from './decrypt';
 export const encryptBlob = encryptMedia;
+export { encryptFile };
